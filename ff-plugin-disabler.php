9d1dee8867cd7042430d8f424ee48e025b380d3b
aliboy08 <alistair.ponce@gmail.com>
2018-02-26 09:32:49 +0800

    added plugin disabler

---
+++ ff-plugin-disabler.php
@@ -0,0 +1,36 @@
+<?php
+$request_uri = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
+
+$is_admin = strpos( $request_uri, '/wp-admin/' );
+
+// add filter in front pages only
+if( false === $is_admin ){
+	add_filter( 'option_active_plugins', 'ff_option_active_plugins' );
+}
+
+function ff_option_active_plugins( $plugins ){
+	
+	global $request_uri;
+	
+	$unnecessary_plugins = array();
+
+	// Deactivate unnecessary plugins on homepage
+	if( $request_uri == '/' ){
+		$unnecessary_plugins[] = 'ajax-thumbnail-rebuild/ajax-thumbnail-rebuild.php';
+		$unnecessary_plugins[] = 'custom-twitter-feed/custom-twitter-feed.php';
+		$unnecessary_plugins[] = 'duplicate-post/duplicate-post.php';
+		$unnecessary_plugins[] = 'fivebyfive-media-boxes/fivebyfive-media-boxes.php';
+		$unnecessary_plugins[] = 'really-simple-captcha/really-simple-captcha.php';
+		$unnecessary_plugins[] = 'wp-smushit/wp-smush.php';
+		$unnecessary_plugins[] = 'wp-pagenavi/wp-pagenavi.php';
+	}
+
+	foreach ( $unnecessary_plugins as $plugin ) {
+		$k = array_search( $plugin, $plugins );
+		if( false !== $k ){
+			unset( $plugins[$k] );
+		}
+	}
+	
+	return $plugins;
+}
